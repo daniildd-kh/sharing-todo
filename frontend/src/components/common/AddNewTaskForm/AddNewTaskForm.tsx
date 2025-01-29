31a6ebceee14cd24db8460281ec8468c455c3eb9
Khromov Daniil <khromov865@mail.ru>
2025-01-29 23:44:12 +0300

    feat: status is separated from the form

--- frontend/src/components/common/AddNewTaskForm/AddNewTaskForm.tsx
+++ frontend/src/components/common/AddNewTaskForm/AddNewTaskForm.tsx
@@ -10,68 +10,7 @@ import { StatusType } from "../../../models";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { boolean, object, ObjectSchema, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-
-interface NewTaskStatusesProps {
-  setStatus: (status: StatusType) => void;
-  currentStatus: StatusType;
-}
-interface IStatus {
-  label: string;
-  value: StatusType;
-  icon: IconName;
-}
-
-const statuses: IStatus[] = [
-  {
-    label: "Без статуса",
-    value: "unfinished",
-    icon: "close",
-  },
-  {
-    label: "В работе",
-    value: "inProgress",
-    icon: "progress",
-  },
-  {
-    label: "На рассмотрение",
-    value: "waitingForApproval",
-    icon: "checkmark",
-  },
-  {
-    label: "Готово",
-    value: "completed",
-    icon: "checkmark",
-  },
-];
-
-const NewTaskStatuses = ({
-  setStatus,
-  currentStatus,
-}: NewTaskStatusesProps) => {
-  return (
-    <div className={style.statuses}>
-      {statuses.map((status, index) => (
-        <div className={style.status} key={index}>
-          <Button
-            type="button"
-            className={clsx(
-              style.button,
-              style.accordionItem,
-              style[status.value]
-            )}
-            onClick={() => setStatus(status.value)}
-          >
-            <IconSvg name={status.icon} size={13} />
-            <SmallText>{status.label}</SmallText>
-          </Button>
-          {currentStatus === status.value && (
-            <IconSvg name="checkmark" size={13} color="#f075aa" />
-          )}
-        </div>
-      ))}
-    </div>
-  );
-};
+import NewTaskStatuses from "./Statuses";
 
 interface AddNewTaskFormProps {
   onClose?: () => void;
