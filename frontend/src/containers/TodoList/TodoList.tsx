e11498e71a8ceebce6b1eb103208da92444decba
Khromov Daniil <khromov865@mail.ru>
2025-03-04 16:32:38 +0300

    feat: todo page refactoring

--- frontend/src/containers/TodoList/TodoList.tsx
+++ frontend/src/containers/TodoList/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   DndContext,
   closestCenter,
@@ -17,19 +17,41 @@ import {
 } from "@dnd-kit/sortable";
 import { Task } from "../../components/common/Task/Task";
 import { ITask } from "../../models";
-import SortableTask from "./SortableTask";
+import SortableTask from "./components/SortableTask";
 import TodoService from "../../services/TodoService";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store/store";
+import { fetchGetUserTasks } from "../../store/actions";
+import { LargeText } from "../../components/common/Typography/Typography";
+import Spinner from "../../components/common/Loader/Spinner";
+import style from "./TodoList.module.scss";
+import TodoHeader from "./components/TodoHeader";
 
-interface TodoListProps {
-  tasksList: ITask[];
-}
+const TodoList = () => {
+  const {
+    loading,
+    tasks: reduxTasks,
+    error,
+  } = useSelector((state: RootState) => state.todo);
+  const originTasks = useRef<ITask[]>([]);
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const dispatch = useDispatch<AppDispatch>();
 
-const TodoList = ({ tasksList }: TodoListProps) => {
-  const [tasks, setTasks] = useState(tasksList);
+  // useEffect(() => {
+  //   setTasks(tasksList);
+  // }, [tasksList]);
 
   useEffect(() => {
-    setTasks(tasksList);
-  }, [tasksList]);
+    dispatch(fetchGetUserTasks());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (reduxTasks) {
+      const sortedTasks = [...reduxTasks].sort((a, b) => a.order - b.order);
+      setTasks(sortedTasks);
+      originTasks.current = sortedTasks;
+    }
+  }, [reduxTasks]);
 
   const sensors = useSensors(
     useSensor(PointerSensor),
@@ -60,30 +82,52 @@ const TodoList = ({ tasksList }: TodoListProps) => {
   };
 
   return (
-    <DndContext
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragEnd={handleDragEnd}
-    >
-      <SortableContext
-        items={tasks.map((task) => ({ id: task._id }))}
-        strategy={verticalListSortingStrategy}
-      >
-        {tasks.map((task) => {
-          return (
-            <SortableTask id={task._id} key={task._id}>
-              <Task
-                _id={task._id}
-                status={task.status}
-                isImportant={task.isImportant}
-                title={task.title}
-                description={task.description}
-              />
-            </SortableTask>
-          );
-        })}
-      </SortableContext>
-    </DndContext>
+    <>
+      <TodoHeader setTasks={setTasks} originTasks={originTasks} />
+      {loading && (
+        <div className={style.centerContent}>
+          <Spinner size={48} />
+        </div>
+      )}
+      {error && (
+        <div className={style.centerContent}>
+          <LargeText style={{ color: "red" }}>{error}</LargeText>
+        </div>
+      )}
+      {tasks?.length === 0 && (
+        <div className={style.centerContent}>
+          <LargeText>Пока нет задач, создайте новую!</LargeText>
+        </div>
+      )}
+      {tasks && (
+        <div className={style.list}>
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={tasks.map((task) => ({ id: task._id }))}
+              strategy={verticalListSortingStrategy}
+            >
+              {tasks.map((task) => {
+                return (
+                  <SortableTask id={task._id} key={task._id}>
+                    <Task
+                      _id={task._id}
+                      status={task.status}
+                      isImportant={task.isImportant}
+                      title={task.title}
+                      description={task.description}
+                    />
+                  </SortableTask>
+                );
+              })}
+            </SortableContext>
+          </DndContext>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ frontend/src/containers/TodoList/components/TodoHeader.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import IconSvg from "../../../components/common/Icons/IconSvg";
+import { Title, Text } from "../../../components/common/Typography/Typography";
+import { Button } from "../../../components/common/Button/Button";
+import style from "./TodoHeader.module.scss";
+import Filter from "../../../pages/TodoPage/components/Filter/Filter";
+import Search from "../../../pages/TodoPage/components/Search/Search";
+import { ITask, StatusType } from "../../../models";
+import { Modal } from "../../../components/common/Modal/Modal";
+import AddNewTaskForm from "../../../components/common/AddNewTaskForm/AddNewTaskForm";
+
+interface TodoHeaderProps {
+  setTasks: React.Dispatch<React.SetStateAction<ITask[]>>;
+  originTasks: React.RefObject<ITask[]>;
+}
+
+const TodoHeader = ({ setTasks, originTasks }: TodoHeaderProps) => {
+  const [sortStatus, setSortStatus] = useState<StatusType>();
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const sortByStatus = (statusName: StatusType) => {
+      setTasks((prevTasks) =>
+        [...prevTasks].sort(
+          (a: ITask, b: ITask) =>
+            (b.status === statusName ? 1 : 0) -
+            (a.status === statusName ? 1 : 0)
+        )
+      );
+    };
+    if (sortStatus) {
+      sortByStatus(sortStatus);
+    }
+  }, [sortStatus]);
+
+  const sortByPriority = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = e.target.checked;
+    setTasks((prevTasks) =>
+      [...prevTasks].sort((a: ITask, b: ITask) => {
+        if (isChecked) {
+          return (b.isImportant ? 1 : 0) - (a.isImportant ? 1 : 0);
+        } else {
+          return a.order - b.order;
+        }
+      })
+    );
+  };
+
+  const handleSearch = (query: string) => {
+    setTasks(
+      [...originTasks.current].filter(
+        (task) =>
+          task.description.toLowerCase().includes(query) ||
+          task.title.toLowerCase().includes(query)
+      )
+    );
+  };
+
+  return (
+    <>
+      <span className={style.title}>
+        <IconSvg name="inbox" size={36} />
+        <Title>Входящие задачи</Title>
+      </span>
+      <div className={style.options}>
+        <Button onClick={() => setShowModal(true)} className={style.buttonTodo}>
+          <IconSvg name="add" />
+          <Text>Добавить задачу</Text>
+        </Button>
+        <Filter
+          classButton={style.buttonTodo}
+          setSortStatus={setSortStatus}
+          sortStatus={sortStatus}
+          sortByPriority={sortByPriority}
+        />
+        <Search onSearch={handleSearch} />
+      </div>
+      <Modal isOpen={showModal} handleClose={() => setShowModal(false)}>
+        <AddNewTaskForm onClose={() => setShowModal(false)} />
+      </Modal>
+    </>
+  );
+};
+
+export default TodoHeader;
